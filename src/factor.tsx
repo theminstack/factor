e450bc833f5710a7c8fb271c1ec9dc1e9512d366
Chris Ackerman <chris@topher.land>
2022-11-08 10:31:06 -0800

    moved factor context to a unique symbol property of the Factor component

--- src/factor.tsx
+++ src/factor.tsx
@@ -13,43 +13,45 @@ import {
 } from './selector.js';
 import { createWorker } from './worker.js';
 
+const $CONTEXT = Symbol.for('$$FactorContext');
+
 type Factor<TValue, TProps extends object> = ComponentType<TProps & { children?: ReactNode }> & {
-  readonly Context: Context<Observable<TValue> | undefined>;
+  readonly [$CONTEXT]: Context<Observable<TValue> | undefined>;
 };
 
 type FactorHook = {
-  <TValue>(factor: Pick<Factor<TValue, any>, 'Context'>): TValue;
+  <TValue>(factor: Pick<Factor<TValue, any>, typeof $CONTEXT>): TValue;
   <TValue, TSelector extends SelectorValue<TValue, any>>(
-    factor: Pick<Factor<TValue, any>, 'Context'>,
+    factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
     selector: TSelector,
     dependencies?: unknown[],
   ): InferSelectedValue<TSelector>;
   <TValue, TSelector extends SelectorArray<TValue>>(
-    factor: Pick<Factor<TValue, any>, 'Context'>,
+    factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
     selector: TSelector,
     dependencies?: unknown[],
   ): InferSelectedArray<TSelector>;
   <TValue, TSelector extends SelectorObject<TValue>>(
-    factor: Pick<Factor<TValue, any>, 'Context'>,
+    factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
     selector: TSelector,
     dependencies?: unknown[],
   ): InferSelectedObject<TSelector>;
 };
 
 type OptionalFactorHook = {
-  <TValue>(factor: Pick<Factor<TValue, any>, 'Context'>): TValue | undefined;
+  <TValue>(factor: Pick<Factor<TValue, any>, typeof $CONTEXT>): TValue | undefined;
   <TValue, TSelector extends SelectorValue<TValue | undefined, any>>(
-    factor: Pick<Factor<TValue, any>, 'Context'>,
+    factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
     selector: TSelector,
     dependencies?: unknown[],
   ): InferSelectedValue<TSelector>;
   <TValue, TSelector extends SelectorArray<TValue | undefined>>(
-    factor: Pick<Factor<TValue, any>, 'Context'>,
+    factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
     selector: TSelector,
     dependencies?: unknown[],
   ): InferSelectedArray<TSelector>;
   <TValue, TSelector extends SelectorObject<TValue | undefined>>(
-    factor: Pick<Factor<TValue, any>, 'Context'>,
+    factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
     selector: TSelector,
     dependencies?: unknown[],
   ): InferSelectedObject<TSelector>;
@@ -59,29 +61,30 @@ const createFactor = <TValue, TProps extends object>(
   useValue: (() => TValue) | ((props: TProps) => TValue),
 ): Factor<TValue, TProps> => {
   const Worker = createWorker(useValue);
+  const Context = createContext<Observable<TValue> | undefined>(undefined);
   const Factor = ({ children, ...props }: TProps & { children?: ReactNode }): JSX.Element => {
     const [observable, onObservable] = useState<Observable<TValue>>();
 
     return (
       <>
         <Worker onObservable={onObservable} useValueProps={props as TProps} />
-        <Factor.Context.Provider value={observable}>{observable && children}</Factor.Context.Provider>
+        <Context.Provider value={observable}>{observable && children}</Context.Provider>
       </>
     );
   };
 
-  Factor.Context = createContext<Observable<TValue> | undefined>(undefined);
-  Factor.Context.displayName = 'Factor';
+  Factor.displayName = 'Factor';
+  Context.displayName = 'Factor';
 
-  return Factor;
+  return Object.assign(Factor, { [$CONTEXT]: Context });
 };
 
 const useFactor: FactorHook = <TValue, TSelector extends Selector<TValue, any>>(
-  factor: Pick<Factor<TValue, any>, 'Context'>,
+  factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
   selector?: TSelector,
   dependencies?: unknown[],
 ): InferSelected<TValue, TSelector | undefined> => {
-  const observable = useContext(factor.Context);
+  const observable = useContext(factor[$CONTEXT]);
 
   if (!observable) {
     throw new Error('factor parent is required');
@@ -91,11 +94,11 @@ const useFactor: FactorHook = <TValue, TSelector extends Selector<TValue, any>>(
 };
 
 const useOptionalFactor: OptionalFactorHook = <TValue, TSelector extends Selector<TValue | undefined, any>>(
-  factor: Pick<Factor<TValue, any>, 'Context'>,
+  factor: Pick<Factor<TValue, any>, typeof $CONTEXT>,
   selector?: TSelector | undefined,
   dependencies?: unknown[],
 ): InferSelected<TValue | undefined, TSelector | undefined> => {
-  const observable = useContext(factor.Context);
+  const observable = useContext(factor[$CONTEXT]);
 
   return useObservable(observable, selector, dependencies);
 };
